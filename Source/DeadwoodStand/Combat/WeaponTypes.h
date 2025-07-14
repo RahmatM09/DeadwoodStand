0602ea25a3eb9a9885afc2f1774b2bc786a6f838
RahmatM09 <rahmuh09@gmail.com>
2025-07-14 08:15:32 +0530

    HitScan Weapon

--- Source/DeadwoodStand/Combat/WeaponTypes.h
+++ Source/DeadwoodStand/Combat/WeaponTypes.h
@@ -5,10 +5,11 @@
 UENUM(BlueprintType)
 enum class EWeaponType : uint8
 {
-	EWT_Pistol     UMETA(DisplayName = "Pistol"),
-	EWT_Rifle      UMETA(DisplayName = "Rifle"),
-	EWT_Shotgun    UMETA(DisplayName = "Shotgun"),
-	EWT_Grenade    UMETA(DisplayName = "Grenade Launcher"),
-	EWT_Melee      UMETA(DisplayName = "Melee"),
-	EWT_MAX        UMETA(Hidden)
+	EWT_NoWeapon     UMETA(DisplayName = "No Weapon"),
+	EWT_Pistol       UMETA(DisplayName = "Pistol"),
+	EWT_Rifle        UMETA(DisplayName = "Rifle"),
+	EWT_Shotgun      UMETA(DisplayName = "Shotgun"),
+	EWT_Grenade      UMETA(DisplayName = "Grenade Launcher"),
+	EWT_Melee        UMETA(DisplayName = "Melee"),
+	EWT_MAX          UMETA(Hidden)
 };
